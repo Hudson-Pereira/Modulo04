46afd0037d2b976dd602fb528de5a0a4ec21e131
Hudson-Pereira <hudson.o.pereira@gmail.com>
2021-11-30 22:01:57 -0300

    meio aula 2

--- Aulas/aula1-nest/primeiro-nest/src/app.module.ts
+++ Aulas/aula1-nest/primeiro-nest/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FilmesModule } from './filmes/filmes.module';
 
 @Module({
-  imports: [FilmesModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
