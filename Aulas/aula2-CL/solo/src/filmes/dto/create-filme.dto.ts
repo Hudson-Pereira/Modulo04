316d6e8519d0dfe775f9135634ec531cbea18b58
Hudson-Pereira <hudson.o.pereira@gmail.com>
2021-12-01 21:49:43 -0300

    prisma instalado

--- Aulas/aula2-CL/solo/src/filmes/dto/create-filme.dto.ts
+++ Aulas/aula2-CL/solo/src/filmes/dto/create-filme.dto.ts
@@ -1,6 +1,19 @@
+import { IsEmpty, IsInt, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateFilmeDto {
+  @IsNotEmpty()
+  @IsString()
   nome: string;
+
+  @IsNotEmpty()
+  @IsInt()
   ano: number;
+
+  @IsNotEmpty()
+  @IsString()
   ator: string;
+
+  @IsNotEmpty()
+  @IsInt()
   duracao: number;
 }
